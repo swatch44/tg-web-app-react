286e5942adb3158bdd36940630bc40c63445a746
unknown <polupok3p@gmail.com>
2023-10-26 15:03:37 +0300

    initial

---
+++ web-app/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+const tg = window.Telegram.WebApp
+
+function App() {
+
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
+  const onClose = () => {
+
+    tg.close ()
+
+  }
+
+  return (
+    <div className="App">
+      <button onClick = {onClose}>Закрыть</button>
+    </div>
+  );
+}
+
+export default App;
